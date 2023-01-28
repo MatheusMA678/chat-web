3f4de3953ef2a44c2dcebad606b58cc8df42765f
Matheus Mattos Avelino <114790698+MatheusMA678@users.noreply.github.com>
2023-01-27 20:55:40 -0400

    update

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,10 +2,11 @@ import { MagnifyingGlass } from "phosphor-react";
 
 import capa from "../assets/images/capa.png"
 import logo from "../assets/images/logo.png"
+import avatar from "../assets/images/avatar.png"
 
 export default function Root() {
   return (
-    <div className="grid grid-cols-[30%_70%] w-screen h-screen bg-background text-white">
+    <div className="md:grid md:grid-cols-[30%_70%] w-screen h-screen bg-background text-white">
       <div className="col-span-1 h-full shadow-2xl drop-shadow-lg flex flex-col bg-background">
         <header className="w-full flex justify-center items-center">
           <img 
@@ -33,7 +34,7 @@ export default function Root() {
           />
           <button 
             type="submit"
-            className="rounded-lg bg-purple-500 py-2 px-4 h-full transition-all active:bg-purple-700 active:ring-2 active:ring-purple-800 hover:bg-purple-600"
+            className="hidden lg:block rounded-lg bg-purple-500 py-2 px-4 h-full transition-all active:bg-purple-700 active:ring-2 active:ring-purple-800 hover:bg-purple-600"
           ><MagnifyingGlass size={18} weight='bold' color='white' /></button>
         </form>
         <nav className=" p-4 flex-1">
@@ -41,25 +42,34 @@ export default function Root() {
             <li>
               <a 
                 href={`/chat/1`}
-                className="block w-full p-2"
-              >Cecilia Sassaki</a>
+                className="flex gap-4 w-full p-2 rounded-lg transition-all hover:bg-gray-700"
+              >
+                <img src={avatar} alt="Foto de Cecilia Sassaki" />
+                <div className="flex flex-col">
+                  <h2>Cecilia Sassaki</h2>
+                  <div className="flex items-center gap-1.5">
+                    <div className="w-2 h-2 rounded-full bg-green-400"/>
+                    <span className="text-green-400 text-sm">Online</span>
+                  </div>
+                </div>
+              </a>
             </li>
           </ul>
         </nav>
         <footer className="w-full">
-          <p className="text-center">Desenvolvido por 
+          <p className="text-center text-gray-500">Desenvolvido por 
             <a 
               href="https://github.com/MatheusMA678"
-              className="font-bold"
+              className="font-bold text-purple-500"
             > Matheus</a>
           </p>
         </footer>
       </div>
-      <div className="p-8">
+      <div className="overflow-hidden p-8 hidden md:block">
         <img 
           src={capa} 
           alt="Capa do Projeto"
-          className="rounded-xl h-full w-full"
+          className="rounded-xl max-h-full w-full"
         />
       </div>
     </div>
