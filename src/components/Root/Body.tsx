f225fab35026a8543b61e34b7ab05fbe0df5a6bf
Matheus Mattos Avelino <114790698+MatheusMA678@users.noreply.github.com>
2023-01-29 12:56:17 -0400

    add Outlet

--- src/components/Root/Body.tsx
+++ src/components/Root/Body.tsx
@@ -1,13 +1,10 @@
-import capa from "../assets/images/capa.png"
+import capa from "../assets/images/capa.png";
+import { Outlet } from "react-router-dom";
 
 export function Body() {
   return (
     <div className="overflow-hidden p-8 hidden md:block">
-      <img 
-        src={capa} 
-        alt="Capa do Projeto"
-        className="rounded-xl max-h-full w-full"
-      />
+      <Outlet />
     </div>
-  )
+  );
 }
